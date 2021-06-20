a18eaa77c0ad7411fef3d896088edb85b2ccfc50
Kael Henrique da Silva <kael_hs@hotmail.com>
2021-06-20 06:30:04 -0300

    Fetching dinamic post route

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,10 +1,17 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+import { useRouter } from 'next/router';
 import { getPrismicClient } from '../../services/prismic';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import Header from '../../components/Header';
 
 import commonStyles from '../../styles/common.module.scss';
+
 import styles from './post.module.scss';
-import Header from '../../components/Header';
+import { FiUser, FiClock } from 'react-icons/fi';
+import { AiOutlineCalendar } from 'react-icons/ai';
 
 interface Post {
   first_publication_date: string | null;
@@ -28,11 +35,68 @@ interface PostProps {
 }
 
 export default function Post( { post }: PostProps ) {
-  return(
-    <>
-      <Header />
 
-    </>
+  const router = useRouter();
+
+  if ( router.isFallback ) {
+    return <p> Carregando...</p>
+  }
+
+  function getReadingTime(): number {
+
+    const regexPattern = /[^\w]/;
+
+    const totalWords = post.data.content.reduce((acc, item) => {
+
+      const totaHeadinglWords = item.heading?.split(regexPattern).length ?? 0;
+
+      const totalBodyWords = item.body.reduce((bodyAcc, bodyItem) => {
+        return bodyAcc + bodyItem.text.split(regexPattern).length;
+      }, 0);
+
+      return acc + totaHeadinglWords + totalBodyWords;
+    }, 0);
+
+    return Math.round(totalWords / 200);
+  }
+
+  return(
+    <article className={styles.container}>
+      <img src={post.data.banner.url} alt={post.data.title} />
+      <h1>{post.data.title}</h1>
+      <div className={styles.info}>
+          <div>
+          <span>
+            <AiOutlineCalendar />
+            <span>
+              { format(new Date(post.first_publication_date), 'dd MM yyyy', {
+                locale: ptBR,
+              }) }
+            </span>
+          </span>
+          <span>
+            <FiUser  />
+            <span>{post.data.author}</span>
+          </span>
+          <span>
+            <FiClock />
+            <span>
+              { getReadingTime()} min
+            </span>
+          </span>
+        </div>
+      </div>
+      <div className={styles.content} >
+          { post.data.content.map( content => {
+            <div key={ (Math.random() * 999999).toString()}>
+              <h2>{content.heading}</h2>
+              <div
+                dangerouslySetInnerHTML={ {__html: RichText.asHtml(content.body)}}
+              ></div>
+            </div>
+          })}
+      </div>
+    </article>
   );
 }
 
@@ -54,7 +118,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   return {
     paths: slugParams,
-    fallback: 'blocking'
+    fallback: true,
   }
 };
 
